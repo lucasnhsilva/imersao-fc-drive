1f60bee2dcfe9f685e4a59e9aa27c93a5d7a714f
lucasnathandev <lucas.develop.prog@gmail.com>
2024-12-10 20:57:11 -0300

    chore: add nestjs config module

--- src/maps/places/places.service.ts
+++ src/maps/places/places.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
-
+import {
+  Client as GoogleMapsClient,
+  PlaceInputType,
+} from '@googlemaps/google-maps-services-js';
 @Injectable()
 export class PlacesService {
-  findPlaces(text: string) {
-    return text;
+  constructor(private readonly googleMapsClient: GoogleMapsClient) {}
+  async findPlaces(text: string) {
+    const { data } = await this.googleMapsClient.findPlaceFromText({
+      params: {
+        input: text,
+        inputtype: PlaceInputType.textQuery,
+        fields: ['place_id', 'formatted_address', 'geometry', 'name'],
+        key: 'AIzaSyCnfq37KricZVez-DGFD63Nec68vLJCE4w',
+      },
+    });
+    return data;
   }
 }
