2750641a57de0917189d6d04ca0f2e7ee944e470
lucasnathandev <lucas.develop.prog@gmail.com>
2024-12-10 23:21:37 -0300

    chore: configure Docker environment for database and application

--- src/routes/routes.module.ts
+++ src/routes/routes.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RoutesService } from './routes.service';
 import { RoutesController } from './routes.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [RoutesController],
-  providers: [RoutesService],
+  providers: [RoutesService, PrismaService],
 })
 export class RoutesModule {}
